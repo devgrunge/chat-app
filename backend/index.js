42db216e528fd564903e4d85e3d4563f174026e9
alexanderJr <alexmarcjr21@gmail.com>
2024-01-22 19:53:59 +0000

    setup socket.io and frontend

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const port = 3001;
 
-app.use(express.json());
-app.use(cors({ origin: true }));
+const server = http.createServer(app);
 
-app.post("/authenticate", async (request, response) => {
-  const { username } = request.body;
-
-  return res.json({ username: username, secret: "sha256" });
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
 });
 
-app.listen(3001);
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} has connected`);
+
+  io.on("disconnect", () => {
+    consoe.log(`User ${socket.id} has disconnected`);
+  });
+});
+
+app.use(cors());
+
+server.listen(port, () => {
+  console.log(`Server Running on port: ${port} `);
+});
